f311a979037fc69676f7b13baf6601b28263e6f5
Andy Zhou <andyzhou417@gmail.com>
2025-07-12 18:15:51 -0400

    Update

--- app/src/main/java/com/lairoflair/pictureurl/ui/screen/notetaking/NoteTakingScreen.kt
+++ app/src/main/java/com/lairoflair/pictureurl/ui/screen/notetaking/NoteTakingScreen.kt
@@ -1,10 +1,13 @@
 package com.lairoflair.pictureurl.ui.screen.notetaking
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import android.content.Context
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import java.io.File
 
 @Composable
 fun NoteTakingScreen(){
