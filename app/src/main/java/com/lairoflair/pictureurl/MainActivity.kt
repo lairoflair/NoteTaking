f636d180d807ca71cfd93ed186ba6fd4b9afd58d
Andy Zhou <andyzhou417@gmail.com>
2025-07-12 18:14:30 -0400

    Start

--- app/src/main/java/com/lairoflair/pictureurl/MainActivity.kt
+++ app/src/main/java/com/lairoflair/pictureurl/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lairoflair.pictureurl.ui.theme.PictureURLTheme
+import com.lairoflair.pictureurl.ui.screen.notetaking.NoteTakingScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
