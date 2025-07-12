81d8c4e4b175904ebae3f5a62d5aaac6f3c74541
Andy Zhou <andyzhou417@gmail.com>
2025-07-12 17:48:13 -0400

    Add missing imports for state management in NoteTakingScreen

--- app/src/main/java/com/lairoflair/pictureurl/ui/screen/notetaking/NoteTakingScreen.kt
+++ app/src/main/java/com/lairoflair/pictureurl/ui/screen/notetaking/NoteTakingScreen.kt
@@ -1,6 +1,10 @@
 package com.lairoflair.pictureurl.ui.screen.notetaking
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun NoteTakingScreen(){
