2c4fb227a4dcaedc2b326665eb35eb310a03dfe6
Andy Zhou <andyzhou417@gmail.com>
2025-07-14 20:54:19 -0400

    Update

--- app/src/main/java/com/lairoflair/pictureurl/ui/screen/notetaking/Navigation.kt
+++ app/src/main/java/com/lairoflair/pictureurl/ui/screen/notetaking/Navigation.kt
@@ -1,2 +1,24 @@
-package com.lairoflair.pictureurl.ui.screen.notetaking
+package com.lairoflair.pictureurl.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NoteAppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(
+                onNewNoteClick = { navController.navigate("new_note") }
+            )
+        }
+        composable("new_note") {
+            NoteTakingScreen(
+                onBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
